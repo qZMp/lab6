03c57f782b551cb6e3e26c964441a9772c512fc3
Elliot Yaghoobia <eyaghoob@ucsd.edu>
2014-02-13 11:33:39 -0800

    ajax lab

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -27,6 +27,27 @@ function addProjectDetails(e) {
 	var idNumber = projectID.substr('project'.length);
 
 	console.log("User clicked on project " + idNumber);
+
+	$.get("/project/" + String(idNumber), getProject);
+
+	console.log("/project/" + String(idNumber));
+}
+
+function getProject(result)
+{
+	console.log(result);
+
+	var id = result['id'];
+
+	var element = "#project" + id + " .details";
+
+	var projectHTML = '<a href="#" class ="project">' + 
+		'<img src="' + result['image'] + '" class="detailsImage">' +
+		'<p>' + result['title'] + '</p>' +
+		'<p><small>' + result['date'] + '</small></p></a>'+
+		'<p>' + result['summary'] +'</p>';
+
+	$(element).html(projectHTML);
 }
 
 /*
@@ -35,4 +56,16 @@ function addProjectDetails(e) {
  */
 function randomizeColors(e) {
 	console.log("User clicked on color button");
+
+	$.get("/palette/", getColor);	
+}
+
+function getColor(result) {
+	var colors = result["colors"]["hex"];
+
+	$('body').css('background-color', colors[0]);
+	$('.thumbnail').css('background-color', colors[1]);
+	$('h1, h2, h3, h4, h5, h5').css('color', colors[2]);
+	$('p').css('color', colors[3]);
+	$('.project img').css('opacity', .75);	
 }
